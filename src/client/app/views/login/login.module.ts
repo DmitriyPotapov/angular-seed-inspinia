3120e63e432e84cea01a38323a3835498ee6aba7
Dmitry Potapov <potapov.dmitry@protonmail.com>
2017-02-14 16:30:45 +0200

    angular material final

--- src/client/app/views/login/login.module.ts
+++ src/client/app/views/login/login.module.ts
@@ -1,11 +1,10 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
-import { MaterialModule } from "@angular/material";
 import {loginComponent} from "./login.component";
 
 @NgModule({
     declarations: [loginComponent],
-    imports     : [BrowserModule,MaterialModule.forRoot()],
+    imports     : [BrowserModule],
 })
 
 export class LoginModule {}
